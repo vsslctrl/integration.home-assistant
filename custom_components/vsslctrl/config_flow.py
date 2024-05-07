f995c1afc2649711f08e74a420e23404193f9249
VSSLCTRL <vsslcontrolled@proton.me>
2024-05-07 17:26:14 +0200

    Figure out zone IDs

--- custom_components/vsslctrl/config_flow.py
+++ custom_components/vsslctrl/config_flow.py
@@ -14,8 +14,19 @@ from homeassistant.exceptions import HomeAssistantError
 
 from vsslctrl import Vssl
 from vsslctrl.discovery import fetch_zone_id_serial
+from vsslctrl.exceptions import VsslCtrlException
 
-from .const import DOMAIN, SERIAL, ZONES, ZONE_1, ZONE_2, ZONE_3, ZONE_4, ZONE_5, ZONE_6
+from .const import (
+    DOMAIN,
+    SERIAL,
+    ZONES,
+    INPUT_1,
+    INPUT_2,
+    INPUT_3,
+    INPUT_4,
+    INPUT_5,
+    INPUT_6,
+)
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -23,14 +34,23 @@ _LOGGER = logging.getLogger(__name__)
 # TODO adjust the data schema to the data that you need
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
-        vol.Required(ZONE_1, default="10.10.30.10"): str,
-        vol.Optional(ZONE_2): str,
-        vol.Optional(ZONE_3): str,
-        vol.Optional(ZONE_4): str,
-        vol.Optional(ZONE_5): str,
-        vol.Optional(ZONE_6): str,
+        vol.Required(INPUT_1, default="10.10.30.10"): str,
+        vol.Optional(INPUT_2): str,
+        vol.Optional(INPUT_3): str,
+        vol.Optional(INPUT_4): str,
+        vol.Optional(INPUT_5): str,
+        vol.Optional(INPUT_6): str,
     }
 )
+
+
+#
+#
+# TODO, a reset button is requred for each zone
+#
+# Discovery
+#
+#
 
 
 class ConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -63,22 +83,22 @@ class ConfigFlow(ConfigFlow, domain=DOMAIN):
                     data_schema=vol.Schema(
                         {
                             vol.Required(
-                                ZONE_1, default=user_input.get(ZONE_1, "")
+                                INPUT_1, default=user_input.get(INPUT_1, "")
                             ): str,
                             vol.Optional(
-                                ZONE_2, default=user_input.get(ZONE_2, "")
+                                INPUT_2, default=user_input.get(INPUT_2, "")
                             ): str,
                             vol.Optional(
-                                ZONE_3, default=user_input.get(ZONE_3, "")
+                                INPUT_3, default=user_input.get(INPUT_3, "")
                             ): str,
                             vol.Optional(
-                                ZONE_4, default=user_input.get(ZONE_4, "")
+                                INPUT_4, default=user_input.get(INPUT_4, "")
                             ): str,
                             vol.Optional(
-                                ZONE_5, default=user_input.get(ZONE_5, "")
+                                INPUT_5, default=user_input.get(INPUT_5, "")
                             ): str,
                             vol.Optional(
-                                ZONE_6, default=user_input.get(ZONE_6, "")
+                                INPUT_6, default=user_input.get(INPUT_6, "")
                             ): str,
                         }
                     ),
@@ -91,28 +111,51 @@ class ConfigFlow(ConfigFlow, domain=DOMAIN):
 
     async def async_step_connect(self, zones):
         """Connect to VSSL"""
-        zone_list = {}
+
+        # First lets try and get the zone serial number and ID so we can validate
+        valid_zones = {}
+        errors = {}
+        vssl_serial = None  # jsut limit to one VSSL device
+        for key, host in zones.items():
+            try:
+                if host:
+                    zone_id, serial = await fetch_zone_id_serial(host)
+                    if zone_id and serial:
+                        if serial not in valid_zones:
+                            valid_zones[serial] = {}
+                        if vssl_serial is None:
+                            vssl_serial = serial
+
+                        valid_zones[serial][zone_id] = host
+                    else:
+                        raise VsslCtrlException
+            except VsslCtrlException as e:
+                errors[key] = f"Error fetching zone info for {host}"
+
+        if errors:
+            print(errors)
+            # SHOW ERROR FORM
+            pass
+
         try:
             vssl = Vssl()
 
-            for key, zone_ip in zones.items():
-                if zone_ip:
-                    zone_id = int(key.lstrip("ZONE_"))
-                    vssl.add_zone(zone_id, zone_ip)
-                    zone_list[zone_id] = zone_ip
+            # Add zones to VSSL device
+            for zone_id, host in valid_zones[vssl_serial].items():
+                vssl.add_zone(int(zone_id), host)
 
             await vssl.initialise()
 
             name = vssl.settings.name
-            data = {SERIAL: vssl.serial, ZONES: zone_list}
+            data = {SERIAL: vssl_serial, ZONES: valid_zones[vssl_serial]}
 
-            await self.async_set_unique_id(vssl.serial)
-
-            await vssl.shutdown()
+            await self.async_set_unique_id(vssl_serial)
 
         except Exception as e:
             _LOGGER.exception(e)
             return self.async_abort(reason="zone_initialisation")
+        finally:
+            await vssl.shutdown()
 
         return self.async_create_entry(title=name, data=data)
 
